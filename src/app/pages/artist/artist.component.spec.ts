ff034b7d6265fe2df11aaa33954a4939c03c94f6
Tiago Dionesto Willrich da Silva <tiagodws@users.noreply.github.com>
2018-09-28 12:50:46 -0300

    Update artist.component.spec.ts

--- src/app/pages/artist/artist.component.spec.ts
+++ src/app/pages/artist/artist.component.spec.ts
@@ -1,6 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ArtistComponent } from './artist.component';
+import { AlbumComponent } from './components/album/album.component';
+import { FeaturedArtistComponent } from './components/featured-artist/featured-artist.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { TrackComponent } from './components/track/track.component';
 
 describe('ArtistComponent', () => {
   let component: ArtistComponent;
@@ -8,14 +16,34 @@ describe('ArtistComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ArtistComponent ]
-    })
-    .compileComponents();
+      imports: [
+        CommonModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+        NoopAnimationsModule,
+      ],
+      declarations: [
+        ArtistComponent,
+        AlbumComponent,
+        TrackComponent,
+        FeaturedArtistComponent,
+        HeroComponent,
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ArtistComponent);
     component = fixture.componentInstance;
+
+    component.artist = {
+      id: 1,
+      bio: 'Bio',
+      name: 'Test',
+      tags: [],
+      url: '',
+    };
+
     fixture.detectChanges();
   });
 
